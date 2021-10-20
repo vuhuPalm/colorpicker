333f73af4e185d59c1cf47cbd14181f1c69db470
vuhuPalm <dpalmer1701@gmail.com>
2021-10-20 08:33:36 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    color: "#FFFFFF",
+    textColor: "#000000"
+  });
+});
+
+module.exports = router;
+router.post('/', function(req, res){
+  res.render('index', {
+    color: req.body.colorPicker,
+    text: req.body.textColorPicker
+  })
+});
+
+router.post('/random', function(req, res){
+  res.render('index', {
+    color: generateRandomHexCode(),
+    textColor: generateRandomHexCode()
+  })
+});
+function generateRandomHexCode() {
+  let hexCode = "#"
+  while ( hexCode.length < 7) {
+    hexCode += (Math.round(Math.random() * 15)).toString(16)
+  }
+  return hexCode
+}
